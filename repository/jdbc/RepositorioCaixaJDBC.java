6a10de304234fc8e3af8c43a87af6d340d9239e4
Leonardo <leolopespeixoto@gmail.com>
2021-10-11 17:01:04 -0300

    update caixa Samuel

--- repository/jdbc/RepositorioCaixaJDBC.java
+++ repository/jdbc/RepositorioCaixaJDBC.java
@@ -1,12 +1,15 @@
 package repository.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import config.FabricaDeConexao;
 import modelo.Caixa;
 import repository.RepositorioCaixa;
 
-public class RepositorioCaixaJDBC extends RepositorioJDBC implements RepositorioCaixa{
+public class RepositorioCaixaJDBC extends RepositorioJDBC implements RepositorioCaixa {
 
 	public RepositorioCaixaJDBC(FabricaDeConexao fabricadeconexoes) {
 		super(fabricadeconexoes);
@@ -14,22 +17,40 @@ public class RepositorioCaixaJDBC extends RepositorioJDBC implements Repositorio
 
 	@Override
 	public void add(Caixa Caixa) {
-		
-		
-		//ver se existe usuario existe com aquele cpf
-		
-		//se o usuario existe voce pega o id dele com  aquele cpf
-		
-		//se o usuario não existe voce cria ele e pega o id dele com aquele cpf
-		
+
+		// ver se existe usuario existe com aquele cpf
+
+		// se o usuario existe voce pega o id dele com aquele cpf
+
+		// se o usuario não existe voce cria ele e pega o id dele com aquele cpf
+
 		// cria um perfilcaixa com id de usuario
-		
+
 	}
 
-	@Override
-	public void update(Caixa caixa) {
-		// TODO Auto-generated method stub
-		
+	public void uptade(Caixa caixa){
+		Connection con = super.getConexao();
+		Boolean jaExisteConexao;
+		if (con == null) {
+			jaExisteConexao = false;
+			super.criarConexao();
+		} else {
+			jaExisteConexao = true;
+		}
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("UPDATE Caixa SET *");
+			
+			
+			
+		}catch (SQLException e){
+			throw new RuntimeException("Não foi possivel alterar os dados do caixa.", e);
+			
+		}finally{
+			if(!jaExisteConexao) {
+				super.fecharConexao();
+			}
+		}
 	}
 
 	@Override
