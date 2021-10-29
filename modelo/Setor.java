30909adac1ac02384c5fd2b6775f0ad453ad556c
SAMN <lagivpan@gmail.com>
2021-10-28 21:26:22 -0300

    Find Repositórios(ainda sendo feito por mim)

--- modelo/Setor.java
+++ modelo/Setor.java
@@ -1,5 +1,16 @@
 package modelo;
 
+import java.util.List;
+
 public class Setor {
-private int id;
+	private int id;
+	private String nome;
+	private List<Produto> produtos;
+	
+	public Setor(String nome, int id, List<Produto> produtos) {
+		this.nome = nome;
+		this.id = id;
+		this.produtos = produtos;
+	}
 }
+
--- repository/jdbc/RepositorioClienteJDBC.java
+++ repository/jdbc/RepositorioClienteJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import config.FabricaDeConexao;
@@ -90,7 +91,7 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 		PreparedStatement ps;
 		try {
 			ps = con.prepareStatement(
-					"UPDATE cliente SET nome=?, sobrenome=?, nomeDeUsuario=?,rua=?,bairro=?,cep=?,numeroDaResidencia=? WHERE id=?"
+					"UPDATE cliente SET nomeDeUsuario=?, sobrenome=?, nomeDeUsuario=?,rua=?,bairro=?,cep=?,numeroDaResidencia=? WHERE id=?"
 					);
 			ps.setString(1, cliente.getNome());
 			ps.setString(2, cliente.getSobrenome());
@@ -108,7 +109,7 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 		}
 	}
 
-	public Cliente find(int id) { // corrigir metodo
+	public Cliente find(int id) { 
 		Connection con = super.getConexao();
 		Boolean jaExisteConexao;
 		if (con == null) {
@@ -125,25 +126,8 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 			ps.setInt(1, id);
 			
 			ResultSet rs = ps.executeQuery();
-			boolean temResultado = rs.next();
 			
-			if(temResultado) {
-				long cpf = rs.getLong(2);
-				String nome = rs.getString(3);
-				String sobrenome = rs.getString(4);
-				String nomeDeUsuario = rs.getString(5);
-				String rua = rs.getString(6);
-				String bairro = rs.getString(7);
-				int cep = rs.getInt(8);
-				int numeroDaResidencia = rs.getInt(9);
-				byte ddd = rs.getByte("DDD");
-				byte ddi = rs.getByte("DDI");
-				int telefone = rs.getInt("numeroTelefone");
-				
-				return new Cliente(id, cpf, nome, sobrenome, nomeDeUsuario, rua, bairro, cep, numeroDaResidencia, ddi, ddd, telefone);
-			}else {
-				throw new RuntimeException("Cliente não cadastrado.");
-			}
+			return lerCliente(rs);
 			
 		} catch (SQLException e) {
 			throw new RuntimeException("Não foi possível encontrar cliente!", e);
@@ -154,7 +138,7 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 		}
 	}
 
-	public Cliente findByCpf(long cpf) { //metodo igual o de getCliente?
+	public Cliente findByCpf(long cpf) { 
 		Connection con = super.getConexao();
 		Boolean jaExisteConexao;
 		if (con == null) {
@@ -171,25 +155,8 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 			ps.setLong(1, cpf);
 			
 			ResultSet rs = ps.executeQuery();
-			boolean temResultado = rs.next();
 			
-			if(temResultado) {
-				int id = rs.getInt(2);
-				String nome = rs.getString(3);
-				String sobrenome = rs.getString(4);
-				String nomeDeUsuario = rs.getString(5);
-				String rua = rs.getString(6);
-				String bairro = rs.getString(7);
-				int cep = rs.getInt(8);
-				int numeroDaResidencia = rs.getInt(9);
-				byte ddd = rs.getByte("DDD");
-				byte ddi = rs.getByte("DDI");
-				int telefone = rs.getInt("numeroTelefone");
-				
-				return new Cliente(id, cpf, nome, sobrenome, nomeDeUsuario, rua, bairro, cep, numeroDaResidencia, ddi, ddd, telefone);
-			}else {
-				throw new RuntimeException("Cliente não cadastrado.");
-			}
+			return lerCliente(rs);
 			
 		} catch (SQLException e) {
 			throw new RuntimeException("Não foi possível encontrar cliente!", e);
@@ -200,8 +167,106 @@ public class RepositorioClienteJDBC extends RepositorioJDBC implements Repositor
 		}
 	}
 
-	public List<Cliente> findByNomeDeUsuario(String nomeDeUsuario) {
-		return null;
+	public Cliente findByNomeDeUsuario(String nomeDeUsuario) {
+		Connection con = super.getConexao();
+		Boolean jaExisteConexao;
+		if (con == null) {
+			jaExisteConexao = false;
+			super.criarConexao();
+		} else {
+			jaExisteConexao = true;
+		}
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(
+					"SELECT * FROM cliente WHERE nomeDeUsuario=?"
+					);
+			ps.setString(1, nomeDeUsuario);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			return lerCliente(rs);
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Não foi possível encontrar cliente!", e);
+		}finally {
+			if(!jaExisteConexao) {
+				super.fecharConexao();
+			}
+		}
+	}
+	
+	public Cliente findByNomeDeUsuarioAndSenha(String nomeDeUsuario, String senha) {
+		Connection con = super.getConexao();
+		Boolean jaExisteConexao;
+		if (con == null) {
+			jaExisteConexao = false;
+			super.criarConexao();
+		} else {
+			jaExisteConexao = true;
+		}
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement("SELECT * FROM cliente WHERE nomeDeUsuario=?, senha=?");
+			ps.setString(1, nomeDeUsuario);
+			ps.setString(2, senha);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			return lerCliente(rs);
+		}catch(SQLException e) {
+			throw new RuntimeException ("Não foi possivel encontrar o cliente!", e);
+		}finally {
+			if(!jaExisteConexao) {
+				super.fecharConexao();
+			}
+		}
+	}
+	
+	public List<Cliente> lerClientes(ResultSet conjuntoDeResultados) throws SQLException { 
+		
+		List<Cliente> clientes = new ArrayList<>(conjuntoDeResultados.getRow());
+		
+		while(conjuntoDeResultados.next()) {
+			
+			int id = conjuntoDeResultados.getInt(2);
+			String nome = conjuntoDeResultados.getString(3);
+			String sobrenome = conjuntoDeResultados.getString(4);
+			String nomeDeUsuario = conjuntoDeResultados.getString(5);
+			String rua = conjuntoDeResultados.getString(6);
+			String bairro = conjuntoDeResultados.getString(7);
+			int cep = conjuntoDeResultados.getInt(8);
+			int numeroDaResidencia = conjuntoDeResultados.getInt(9);
+			byte ddd = conjuntoDeResultados.getByte("DDD");
+			byte ddi = conjuntoDeResultados.getByte("DDI");
+			int telefone = conjuntoDeResultados.getInt("numeroTelefone");
+			
+			clientes.add(new Cliente(numeroDaResidencia, id, nome, sobrenome, nomeDeUsuario, rua, bairro, cep, numeroDaResidencia, ddi, ddd, telefone));
+		}
+		return clientes;
+	}
+	
+	public Cliente lerCliente(ResultSet conjuntoDeResultados) throws SQLException {
+		
+		if(conjuntoDeResultados.next()) {
+			
+			int id = conjuntoDeResultados.getInt(2);
+			String nome = conjuntoDeResultados.getString(3);
+			String sobrenome = conjuntoDeResultados.getString(4);
+			String nomeDeUsuario = conjuntoDeResultados.getString(5);
+			String rua = conjuntoDeResultados.getString(6);
+			String bairro = conjuntoDeResultados.getString(7);
+			int cep = conjuntoDeResultados.getInt(8);
+			int numeroDaResidencia = conjuntoDeResultados.getInt(9);
+			byte ddd = conjuntoDeResultados.getByte("DDD");
+			byte ddi = conjuntoDeResultados.getByte("DDI");
+			int telefone = conjuntoDeResultados.getInt("numeroTelefone");
+		
+			return new Cliente(numeroDaResidencia, id, nome, sobrenome, nomeDeUsuario, rua, bairro, cep, numeroDaResidencia, ddi, ddd, telefone);
+		}else {
+			throw new RuntimeException("Cliente não encontrado");
+		}
+		
 	}
 	
 }
