6c1c7214d73e204a25f0917877949318696dc575
Alex <oliveiraalex2004@gmail.com>
2021-10-29 21:33:33 -0300

    Find setor finalizado

--- teste/jUnit/funcional/bancoDeDados/TesteDeRecuperacao.java
+++ teste/jUnit/funcional/bancoDeDados/TesteDeRecuperacao.java
@@ -13,13 +13,17 @@ import org.junit.jupiter.api.function.Executable;
 
 import modelo.Cliente;
 import modelo.Fornecedor;
+import modelo.Gerente;
 import modelo.Produto;
 import modelo.Setor;
+import modelo.Vendedor;
 import repository.FabricaDeRepositorios;
 import repository.RepositorioCliente;
 import repository.RepositorioFornecedor;
+import repository.RepositorioGerente;
 import repository.RepositorioProduto;
 import repository.RepositorioSetor;
+import repository.RepositorioVendedor;
 import repository.jdbc.RepositorioJDBC;
 import teste.jUnit.ConteudoTabelaDB;
 import teste.jUnit.MapaRegistro;
@@ -55,6 +59,8 @@ public class TesteDeRecuperacao {
 		List<Setor> setoresEsperados = conteudoTabelaDB.getRegistros();
 		RepositorioSetor repositorio = fabricaDeRepositorios.criarRepositorioSetor();
 
+		iniciarConecxaoSePreciso(repositorio);
+
 		List<Executable> listaDeAssercoes = new ArrayList<>(setoresEsperados.size());
 
 		for (Setor setorEsperado : setoresEsperados) {
@@ -74,9 +80,9 @@ public class TesteDeRecuperacao {
 		ConteudoTabelaDB<Cliente> conteudoTabelaDB = mapaRegistros.get(Cliente.class);
 		List<Cliente> clientesEsperados = conteudoTabelaDB.getRegistros();
 		RepositorioCliente repositorio = fabricaDeRepositorios.criarRepositorioCliente();
-		
+
 		iniciarConecxaoSePreciso(repositorio);
-		
+
 		List<Executable> listaDeAssercoes = new ArrayList<>(clientesEsperados.size());
 
 		for (Cliente clienteEsperado : clientesEsperados) {
@@ -107,40 +113,88 @@ public class TesteDeRecuperacao {
 			Fornecedor fornecedorEncontrado = repositorio.find(fornecedorEsperado.getId());
 
 			listaDeAssercoes.add(() -> {
+
 				Assertions.assertEquals(fornecedorEsperado, fornecedorEncontrado,
 						"\nFornecedor encontrado diferente: \n" + fornecedorEncontrado + " \nDeveria ser: \n"
 								+ fornecedorEsperado + '\n');
 			});
 
 		}
-		Assertions.assertAll(listaDeAssercoes);
 	}
 	
-	@Test
-	public void findProduto() {
-		ConteudoTabelaDB<Produto> conteudoDaTabelaProduto = mapaRegistros.get(Produto.class);	
-		List<Produto> produtosEsperados = conteudoDaTabelaProduto.getRegistros();
-		RepositorioProduto repositorio = fabricaDeRepositorios.criarRepositorioDeProduto();
+	
+	public void findGerente() {
+		ConteudoTabelaDB<Gerente> conteudoDaTabelaGerente = mapaRegistros.get(Gerente.class);
+		List<Gerente> gerentesEsperados = conteudoDaTabelaGerente.getRegistros();
+		RepositorioGerente repositorio = fabricaDeRepositorios.criarRepositorioGerente();
 		
 		iniciarConecxaoSePreciso(repositorio);
-		List<Executable> listaDeAssercoes = new ArrayList<>(produtosEsperados.size());
 		
-		for(Produto produtoEsperado : produtosEsperados) {
+		List<Executable> listaDeAssercoes = new ArrayList<>(gerentesEsperados.size());
+
+		for (Gerente gerenteEsperado : gerentesEsperados) {
+
+			Gerente gerenteEncontrado = repositorio.find(gerenteEsperado.getId());
+
+			listaDeAssercoes.add(() -> {
+
+				Assertions.assertEquals(gerenteEsperado, gerenteEncontrado, "\nGerente encontrado diferente: \n"
+						+ gerenteEncontrado + " \nDeveria ser: \n" + gerenteEsperado + '\n');
+			});
+		}
+	}
+
+	@Test
+	public void findVendedor() {
+
+		ConteudoTabelaDB<Vendedor> conteudoDaTabelaVendedor = mapaRegistros.get(Vendedor.class);
+		List<Vendedor> vendedoresEsperados = conteudoDaTabelaVendedor.getRegistros();
+		RepositorioVendedor repositorio = fabricaDeRepositorios.criarRepositorioVendedor();
+
+		iniciarConecxaoSePreciso(repositorio);
+
+		List<Executable> listaDeAssercoes = new ArrayList<>(vendedoresEsperados.size());
+
+		for (Vendedor vendedorEsperado : vendedoresEsperados) {
+
+			Vendedor vendedorEncontrado = repositorio.find(vendedorEsperado.getId());
+
+			listaDeAssercoes.add(() -> {
+
+				Assertions.assertEquals(vendedorEsperado, vendedorEncontrado, "\nVendedor encontrado diferente: \n"
+						+ vendedorEncontrado + " \nDeveria ser: \n" + vendedorEsperado + '\n');
+			});
+
+		}
+
+	}
+
+	@Test
+	public void findProduto() {
+		ConteudoTabelaDB<Produto> conteudoDaTabelaProduto = mapaRegistros.get(Produto.class);
+		List<Produto> produtosEsperados = conteudoDaTabelaProduto.getRegistros();
+		RepositorioProduto repositorio = fabricaDeRepositorios.criarRepositorioDeProduto();
+
+		iniciarConecxaoSePreciso(repositorio);
+		List<Executable> listaDeAssercoes = new ArrayList<>(produtosEsperados.size());
+
+		for (Produto produtoEsperado : produtosEsperados) {
 			Produto produtoEncontrado = repositorio.find(produtoEsperado.getId());
-			
-			listaDeAssercoes.add(new Executable(){
+
+			listaDeAssercoes.add(new Executable() {
 				public void execute() throws Throwable {
-					Assertions.assertEquals(produtoEsperado, produtoEncontrado, "\nProduto encontrado diferente: \n" + produtoEncontrado + "\nDeveria ser: \n" + produtoEsperado + "\n");
+					Assertions.assertEquals(produtoEsperado, produtoEncontrado, "\nProduto encontrado diferente: \n"
+							+ produtoEncontrado + "\nDeveria ser: \n" + produtoEsperado + "\n");
 				}
 			});
 		}
 		Assertions.assertAll(listaDeAssercoes);
 	}
 
-	public void iniciarConecxaoSePreciso(Object objeto) {
-		
-		if(objeto instanceof RepositorioJDBC) {
-			((RepositorioJDBC)objeto).criarConexao();
+	private void iniciarConecxaoSePreciso(Object objeto) {
+
+		if (objeto instanceof RepositorioJDBC) {
+			((RepositorioJDBC) objeto).criarConexao();
 		}
 	}
 
