ee2eef6f0054352f0dc850134e9c4c9a6d6d8018
Alex <oliveiraalex2004@gmail.com>
2021-11-04 20:10:26 -0300

    Teste de recuperação deu certo

--- repository/jdbc/RepositorioSetorJDBC.java
+++ repository/jdbc/RepositorioSetorJDBC.java
@@ -3,6 +3,7 @@ package repository.jdbc;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +13,12 @@ import modelo.Produto;
 import modelo.Setor;
 import repository.RepositorioSetor;
 
-public class RepositorioSetorJDBC extends RepositorioJDBC implements RepositorioSetor{
+public class RepositorioSetorJDBC extends RepositorioJDBC implements RepositorioSetor {
 
 	public RepositorioSetorJDBC(FabricaDeConexao fabricadeconexoes) {
 		super(fabricadeconexoes);
 	}
-		
+
 	public void add(Setor model) {
 		Connection con = super.getConexao();
 		Boolean jaExisteConexao;
@@ -28,7 +29,7 @@ public class RepositorioSetorJDBC extends RepositorioJDBC implements Repositorio
 			jaExisteConexao = true;
 		}
 		PreparedStatement ps;
-			
+
 	}
 
 	public Setor findByNome(String nome) {
@@ -42,23 +43,24 @@ public class RepositorioSetorJDBC extends RepositorioJDBC implements Repositorio
 		}
 		PreparedStatement ps;
 		try {
-			ps = con.prepareStatement("SELECT produto.*, setor.id AS idSetor, setor.nome AS nomeSet FROM setor LEFT JOIN produtos WHERE setor.id=? ");
+			ps = con.prepareStatement(
+					"SELECT produto.*, setor.id AS idSetor, setor.nome AS nomeSet FROM setor LEFT JOIN produtos WHERE setor.id=? ");
 			ps.setString(1, nome);
 			ps.execute();
-			
+
 			ResultSet rs = ps.executeQuery();
-			
+
 			return lerSetor(rs);
-		
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			throw new RuntimeException("Não foi possivel encontrar o produto.", e);
-		}finally {
+		} finally {
 			if (!jaExisteConexao) {
 				super.fecharConexao();
 			}
 		}
 	}
-	
+
 	public Setor find(int id) {
 		Connection con = super.getConexao();
 
@@ -73,16 +75,17 @@ public class RepositorioSetorJDBC extends RepositorioJDBC implements Repositorio
 		PreparedStatement ps = null;
 
 		try {
-			
-			ps = con.prepareStatement("SELECT produto.*, setor.id AS idSetor, setor.nome AS nomeSet FROM setor LEFT JOIN produto ON setor.id = produto.id WHERE setor.id=?;");
-			
+
+			ps = con.prepareStatement(
+					"SELECT produto.*, setor.id AS idSetor, setor.nome AS nomeSetor FROM setor LEFT JOIN produto ON setor.id = produto.idSetor WHERE setor.id=?;");
+
 			ps.setInt(1, id);
-			
+
 			ResultSet rs = ps.executeQuery();
-			
+
 			return lerSetor(rs);
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			throw new RuntimeException("Operação não pode ser concluida", e);
 		} finally {
 			if (!jaExisteConexao) {
@@ -90,8 +93,7 @@ public class RepositorioSetorJDBC extends RepositorioJDBC implements Repositorio
 			}
 		}
 	}
-	
-	
+
 	public void updade(int id) {
 		Connection con = super.getConexao();
 
@@ -104,47 +106,45 @@ public class RepositorioSetorJDBC extends RepositorioJDBC implements Repositorio
 		}
 
 		PreparedStatement ps = null;
-		
+
 		try {
 			ps = con.prepareStatement("UPDATE ");
 		} catch (Exception e) {
-			
+
 			throw new RuntimeException(e);
 		}
 	}
 
-public Setor lerSetor(ResultSet conjuntoDeResultados) throws SQLException {
-		
-		if(conjuntoDeResultados.next()) {
+	public Setor lerSetor(ResultSet conjuntoDeResultados) throws SQLException {
+	
+		if (conjuntoDeResultados.next()) {
 
-			int id = conjuntoDeResultados.getInt("idSet");
+			int id = conjuntoDeResultados.getInt("idSetor");
 			String nome = conjuntoDeResultados.getString("nomeSetor");
 			String strIdProduto = conjuntoDeResultados.getString("id");
 			int atualId = id;
-			
+
 			List<Produto> produtos = new ArrayList<>(conjuntoDeResultados.getRow());
-			
-			if(strIdProduto != null) {
+
+			if (strIdProduto != null) {
 				Produto produto = RepositorioProdutoJDBC.apenasLerProduto(conjuntoDeResultados);
 				produtos.add(produto);
 			}
-			
-			while(conjuntoDeResultados.next() && id == atualId) {
-				if(strIdProduto != null) {
+
+			while (conjuntoDeResultados.next() && id == atualId) {
+				if (strIdProduto != null) {
 					Produto produto = RepositorioProdutoJDBC.apenasLerProduto(conjuntoDeResultados);
 					produtos.add(produto);
 				}
 				atualId = conjuntoDeResultados.getInt("idSetor");
 			}
-		
+
 			return new Setor(id, nome, produtos);
 
-			
 		} else {
-			
+
 			throw new RuntimeException("Setor não encontrado");
 		}
 	}
-	
-	
+
 }
